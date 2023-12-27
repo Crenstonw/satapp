12955caa1a50632acdc41de2933e58670f09cc11
Crenstonw <b2016rubensa@gmail.com>
2023-12-27 13:55:49 +0100

    primer commit

---
+++ src/test/java/com/triana/salesianos/edu/satapp/SatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triana.salesianos.edu.satapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
