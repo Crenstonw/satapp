2df2eac78918009066c58e3d6482199a357ab9fd
Crenstonw <b2016rubensa@gmail.com>
2024-01-11 20:18:43 +0100

    optimización del código

--- src/main/java/com/triana/salesianos/edu/satapp/ticket/service/TicketService.java
+++ src/main/java/com/triana/salesianos/edu/satapp/ticket/service/TicketService.java
@@ -1,6 +1,5 @@
 package com.triana.salesianos.edu.satapp.ticket.service;
 
-import com.triana.salesianos.edu.satapp.inventariable.dto.InventariableDto;
 import com.triana.salesianos.edu.satapp.inventariable.modal.Inventariable;
 import com.triana.salesianos.edu.satapp.inventariable.repo.InventariableRepository;
 import com.triana.salesianos.edu.satapp.ticket.dto.CreateTicketRequest;
@@ -10,7 +9,6 @@ import com.triana.salesianos.edu.satapp.ticket.exception.TicketNotFoundException
 import com.triana.salesianos.edu.satapp.ticket.modal.State;
 import com.triana.salesianos.edu.satapp.ticket.modal.Ticket;
 import com.triana.salesianos.edu.satapp.ticket.repo.TicketRepository;
-import com.triana.salesianos.edu.satapp.user.dto.UserResponse;
 import com.triana.salesianos.edu.satapp.user.modal.User;
 import com.triana.salesianos.edu.satapp.user.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +16,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
